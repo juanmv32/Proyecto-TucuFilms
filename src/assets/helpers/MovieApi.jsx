9168bc4d1e481d0ba76be05c76fa669aaa149557
juanmv32 <juan.m.vargas32@gmail.com>
2025-12-06 16:20:16 -0300

    se crea el fetch para la api, junto a props de MovieApi

--- src/assets/helpers/MovieApi.jsx
+++ src/assets/helpers/MovieApi.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const movieApi = () => {
-    
+// sort puede ser
+// 'popularity' por popularidad
+// 'vote_average' mas "popular en votos"
+// 'vote_count' mas votada "cantidad de votos"
+// 'title' alfabetico 
 
-  return (
-    <div>movieApi</div>
-  )
-}
+// type puede ser
+// 'tv'
+// 'movie'
 
-export default movieApi
+// order
+// desc
+// asc
+const MovieApi = ({
+  type = "movie",
+  page = 1,
+  sort = "popularity",
+  order = "desc",
+}) => {
+  const [movies, setMovies] = useState([]);
+
+  
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/discover/${type}?api_key=773243072698134e8a05b7f35c91fafb&language=es-AR&sort_by=${sort}.${order}&page=${page}`
+      );
+      const data = await res.json();
+
+      const moviesVisible = data.results.map((movie) => ({
+        ...movie,
+        visible: true,
+      }));
+
+      setMovies(moviesVisible);
+    };
+
+    fetchMovies();
+  }, [page,sort,order]);
+
+return (
+    <div>
+      <pre>{JSON.stringify(movies, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default MovieApi;
+
+// lo estoy probando en layout/pageslayout
+//  <MovieApi type="movie" sort="popularity"/>
