44aff46712affe94a008252c8060148fc53eaeda
Martin <marticicarodriguez@gmail.com>
2025-12-21 13:09:28 -0300

    intalamos reacthookform

--- src/components/ModalEdit.jsx
+++ src/components/ModalEdit.jsx
@@ -1,25 +1,44 @@
 import React from 'react'
+import { UserContext } from '../contexto/ContextoBD';
+import  { use, useContext, useState } from 'react'
 
 export default function ModalEdit() {
+   
+
+ const { user, actualizarPelicula } = useContext(UserContext);
+   
+const agregarPelicula = ()=>{
+      const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()  
+}
+     
+    
+const onSubmit = (data) => console.log(data)    
+   
+   
+   
   return (
     <>
+   
 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Launch demo modal
 </button>
 
-
 <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className=" modal-dialog modal-dialog-centered">
-    <div className="modal-content bg-dark">
+    <form className="modal-content bg-dark" onSubmit={handleSubmit(onSubmit)}>
       <div className="modal-header text-light">
         <h1 className="modal-title fs-5" id="exampleModalLabel">Agregar/Editar</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-       <form>
+       <div>
        <div className="mb-3">
         <label for="exampleInputEmail1" className="form-label text-light">Nombre</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" defaultValue="test" {...register("example")} />
         </div>
          <div className="mb-3">
          <label for="exampleInputPassword1" className="form-label text-light">Categoria</label>
@@ -30,7 +49,6 @@ export default function ModalEdit() {
             <option value="Ingeniero/a">Comedia</option>
             <option value="Tester">Tester</option>
             <option value="Otro">Otro</option>
-
         </select>
         </div>
         <div className="mb-3">
@@ -41,14 +59,14 @@ export default function ModalEdit() {
        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
        <label className="form-check-label text-light" for="exampleCheck1">Publicado</label>
        </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-     </form>
+      
+     </div>
       </div>
       <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" className="btn btn-primary">Guardar</button>
       </div>
-    </div>
+    </form>
   </div>
 </div>
 
